58fbd0edf2ca59c37b0b1da9eaffd525addc25a7
fangming <fangmingdev@163.com>
2017-03-11 03:26:53 +0800

    添加权限验证类

---
+++ FsmflyingOfSpring/src/com/fsmflying/spring/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,16 @@
+package com.fsmflying.spring.service.impl;
+
+import com.fsmflying.sys.dm.helper.User;
+import com.fsmflying.sys.service.AuthenticationService;
+
+
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+	@Override
+	public User getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
